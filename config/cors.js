af0a83a4117a86bff75c5b96b86efa3df833be41
kartikay_rana <kartik65361@gmail.com>
2023-12-26 03:05:16 +0530

    some issue is resolved

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,5 @@
 export default (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
